f1e5be9e45dc974c215dea27cf05c34645418837
kissthebug <108880176+kissthebug@users.noreply.github.com>
2025-08-27 08:45:09 -0500

    chore: update contents

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
+          <Link href="https://github.com/kissthebug/blog">
             Tailwind Nextjs Theme
           </Link>
         </div>
